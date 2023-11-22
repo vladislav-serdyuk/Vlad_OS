0aa731335723ac6f53a81b89470aab73440dd68c
Vlad <137103197+vladislav-serdyuk@users.noreply.github.com>
2023-11-22 20:48:37 +0300

    Update desktop.py

--- desktop.py
+++ desktop.py
@@ -17,8 +17,21 @@ class Desktop:
         Создаёт рабочий стол на холсте.
         :param c: холст, на котором создаётся рабочий стол.
         """
-        with open("config.json") as config_file:
-            config = json.load(config_file)
+
+        with open('log.txt', 'a') as file:
+            _date: str = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+            file.write(f'{_date}: INFO: desktop_init_start\n')
+
+        try:
+            with open("config.json") as config_file:
+                config = json.load(config_file)
+        except FileNotFoundError:
+            config = {
+                "canvas_width": 1600,
+                "canvas_height": 900,
+                "panel_h": 40,
+                "background": "imgs/desktop/desktop2.png"
+            }
 
         try:
             self.image: PhotoImage = PhotoImage(file=config['background'])
@@ -27,5 +40,9 @@ class Desktop:
                 _date: str = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
                 file.write(f'{_date}: failedToDesktopImage_theImageMayNotExist: {e}\n')
             exit(-1)
+
+        with open('log.txt', 'a') as file:
+            _date: str = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+            file.write(f'{_date}: INFO: desktop_init_end\n')
 
         c.create_image(0, 0, image=self.image, anchor='nw')
