28cfb14432ac4977f01e08215f9351c4362040e2
Vlad <137103197+vladislav-serdyuk@users.noreply.github.com>
2024-01-11 20:55:12 +0300

    Update create_new_user_manager.py

--- create_new_user_manager.py
+++ create_new_user_manager.py
@@ -5,28 +5,29 @@ from datetime import datetime
 
 
 class NewUser:
-    def __init__(self):
-        self.root = tkinter.Toplevel()
-        self.root.resizable(True, False)
-        self.root.title('Create new user manager')
+    def __init__(self, root: tkinter.Tk):
+        self.root = root
+        self._root = tkinter.Toplevel(root)
+        self._root.resizable(True, False)
+        self._root.title('Create new user manager')
 
         with open('log.txt', 'a') as file:
             _date: str = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
             file.write(f'{_date}: INFO: newuser_init_start\n')
 
         for r in range(3):
-            self.root.grid_rowconfigure(r, weight=1)
+            self._root.grid_rowconfigure(r, weight=1)
 
         for c in range(3):
-            self.root.grid_columnconfigure(c, weight=1)
+            self._root.grid_columnconfigure(c, weight=1)
 
-        ttk.Label(self.root, text='Name').grid(column=0, row=0)
-        self.name_entry = ttk.Entry(self.root)
+        ttk.Label(self._root, text='Name').grid(column=0, row=0)
+        self.name_entry = ttk.Entry(self._root)
         self.name_entry.grid(column=1, row=0, columnspan=2, sticky='NSEW')
-        ttk.Label(self.root, text='Password').grid(column=0, row=1)
-        self.password_entry = ttk.Entry(self.root)
+        ttk.Label(self._root, text='Password').grid(column=0, row=1)
+        self.password_entry = ttk.Entry(self._root)
         self.password_entry.grid(column=1, row=1, columnspan=2, sticky='NSEW')
-        ttk.Button(self.root, text='Create', command=self.create).grid(column=2, row=2, sticky='NSEW')
+        ttk.Button(self._root, text='Create', command=self.create).grid(column=2, row=2, sticky='NSEW')
 
         with open('log.txt', 'a') as file:
             _date: str = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
@@ -40,8 +41,9 @@ class NewUser:
             '}'
         with open('user_config.json', 'w') as file:
             file.write(default_config)
-        self.root.destroy()
+        self._root.destroy()
 
         with open('log.txt', 'a') as file:
             _date: str = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
             file.write(f'{_date}: INFO: newuser_create_user\n')
+        self.root.destroy_and_start_explorer()
